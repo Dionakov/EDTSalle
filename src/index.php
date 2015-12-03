f05efd43d4dd3544f567ec625bc1570a7d1284eb
Roch Dionnet <roch.dionnet@univ-lyon1.fr>
2015-12-03 16:37:49 +0100

    Added bootstrap and jquery files. Created page template.

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Index temporaire</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+	</head>
+	<body>
+		<p>Cette page est destinée a être une base de code pour de futures pages plus élaborées.</p>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
